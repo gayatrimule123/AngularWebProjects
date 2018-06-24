b90c9d3dac01edf6e364b2a044d0f6ce5fc627f4
Gayatri Mule <40348907+gayatrimule123@users.noreply.github.com>
2018-06-24 15:21:16 +0530

    3 Components with different color

---
+++ TwelveApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
+import { ThirdCompComponent } from './third-comp/third-comp.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstCompComponent,
+    SecondCompComponent,
+    ThirdCompComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
